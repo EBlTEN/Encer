58e528f33b0a572fc86407c1597b092a6c9c592a
EBlTEN <akana1@outlook.jp>
2023-02-23 11:51:58 +0900

    変数のtypoを修正

--- checker.py
+++ checker.py
@@ -3,5 +3,5 @@ import discord
 owner_id = [501340685757186059, 585858021415190550]
 
 
-def is_owner(interaction: discord.Integration) -> bool:
+def is_owner(interaction: discord.Interaction) -> bool:
     return interaction.user.id in owner_id
