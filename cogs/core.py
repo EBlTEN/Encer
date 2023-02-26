bd779fea748ecb668ccdb5757d00727ce50fc320
EBlTEN <akana1@outlook.jp>
2023-02-27 01:23:36 +0900

    helpコマンドにコマンドとグループの数を表示

--- cogs/core.py
+++ cogs/core.py
@@ -19,7 +19,7 @@ class Core(commands.GroupCog, group_name="encer"):
     @commands.Cog.listener()
     async def on_ready(self):
         logger.info("%s is loaded", __name__)
-        await self.bot.change_presence(activity=discord.Streaming("/encer help"))
+        await self.bot.change_presence(activity=discord.Game(name="/encer help"))
 
     @app_commands.command(description="このbotについて表示する")
     async def about(self, interaction: discord.Interaction):
@@ -33,12 +33,14 @@ class Core(commands.GroupCog, group_name="encer"):
     async def help(self, interaction: discord.Interaction):
         def get_group_command_help_text(group: discord.app_commands.Group):
             cmd_name = ""
+            group_cmd_count = 0
             group_name = get_command_help_text(group)  # グループ名を取得
             for cmd in group.walk_commands():  # グループ内のコマンドを取得
                 # cmd_nameに追記していく
                 cmd_help_text = get_command_help_text(cmd)
                 cmd_name += f"`{cmd_help_text[0]}`:{cmd_help_text[1]}\n"
-            return (group_name[0], cmd_name)
+                group_cmd_count += 1
+            return (group_name[0], cmd_name, group_cmd_count)
 
         def get_command_help_text(cmd: discord.app_commands.Group | discord.app_commands.Command):
             cmd_name = cmd.name
@@ -47,19 +49,27 @@ class Core(commands.GroupCog, group_name="encer"):
                 cmd_description = cmd.description
             return (cmd_name, cmd_description)
 
-        embed = discord.Embed(title="Encer Help",
-                              description="コマンド数`None`\nグループ数`None`")
+        cmd_count = 0
+        group_count = 0
+
+        embed = discord.Embed(title="Encer Help")
+        embed.set_footer(text=f"Encer.commands.Core")
         commands = self.bot.tree.get_commands()
         for cmd in commands:
             # インスタンスを取得して条件分岐
             if isinstance(cmd, discord.app_commands.Group):  # グループの場合
+                group_count += 1
                 cmd_info = get_group_command_help_text(cmd)
                 embed.add_field(
                     name=cmd_info[0], value=f"{cmd_info[1]}", inline=False)
+                cmd_count += cmd_info[2]
             else:  # 単体の場合
+                cmd_count += 1
                 cmd_info = get_command_help_text(cmd)
-                embed.add_field(name=cmd_info[0], value=cmd_info[1])
+                embed.add_field(name=cmd_info[0],
+                                value=cmd_info[1], inline=True)
 
+        embed.description = f"コマンド数:`{cmd_count}`\nグループ数:`{group_count}`"
         await interaction.response.send_message(embed=embed)
 
 
